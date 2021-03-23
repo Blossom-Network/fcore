d8c138b76bebc1700d63ae66cb90baca4429957d
feezee <cubasa4@gmail.com>
2021-03-23 02:03:45 +0100

    disable scoreboard

--- lua/fcore/modules/watermark/cl_watermark.lua
+++ lua/fcore/modules/watermark/cl_watermark.lua
@@ -2,5 +2,5 @@ hook.Add("HUDPaint", "FCore::Watermark", function()
     draw.RoundedBox(4, 8, 8, 100, 24, FCore.HUD.Config.Colors.secondary)
 
     draw.DrawText(utf8.char(0xf126), "FCore_FontAwesome_14_500", 16, 13, FCore.HUD.Config.Colors.white, TEXT_ALIGN_LEFT)
-    draw.DrawText("22/03/2021", "FCore_Open Sans_14_500", 62, 12, FCore.HUD.Config.Colors.text, TEXT_ALIGN_CENTER)
+    draw.DrawText("fcore/main", "FCore_Open Sans_14_500", 62, 12, FCore.HUD.Config.Colors.text, TEXT_ALIGN_CENTER)
 end)
