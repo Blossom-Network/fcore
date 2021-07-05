b2cf6b45ea0101dff127ce5b1910e6ce1348e4a2
feezee <cubasa4@gmail.com>
2021-07-05 18:50:52 +0200

    loading gang data

--- lua/fcore/modules/gangs/cl_data.lua
+++ lua/fcore/modules/gangs/cl_data.lua
@@ -3,6 +3,18 @@ FCore.Gangs.Instances = FCore.Gangs.Instances or {}
 
 function FCore.Gangs.LoadGangs()
     FCore.Gangs.Instances = net.ReadTable()
+
+    for gangid,gangdata in ipairs(FCore.Gangs.Instances) do
+        if !gangdata.active then continue end
+
+        for memberid, memberdata in pairs(gangdata.members) do
+            local ply = player.GetBySteamID64(memberid)
+
+            if ply then
+                ply:SetGang(gangid)
+            end
+        end
+    end
 end
 
 function FCore.Gangs.Update()
